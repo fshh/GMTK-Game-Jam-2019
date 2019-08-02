3936d1614276826c7f8e19234dd7a546317f762c
fshh <knollmeyer.a@husky.neu.edu>
2019-08-02 18:51:08 -0400

    Basic player movement; folders are filled

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 4f;
+    [SerializeField][Range(0f, 1f)] private float acceleration = 0.3f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate() {
+        //rb.position += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed, acceleration);
+    }
+}
