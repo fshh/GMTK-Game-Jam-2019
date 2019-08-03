f2b7305999732b58535019c2d24be1714f99faf6
Max Fortna <33673420+thexammer@users.noreply.github.com>
2019-08-03 19:36:42 -0400

    Add Pause and Death Menu

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
+    [SerializeField] private int digits = 10;
+
     private int score = 0;
     public int Score
     {
@@ -16,7 +19,22 @@ public class ScoreBoard : MonoBehaviour
         set
         {
             score = value;
-            text.text = score.ToString();
+            StringBuilder scoreString = new StringBuilder(); 
+            for (int i = 0; i < digits - score.ToString().Length; i++)
+            {
+                scoreString.Append(0);
+            }
+            if (digits - score.ToString().Length < 0)
+            {
+                // Holy shit you spent some fucking time playing this game
+                scoreString.Append("WTF HOW");
+
+            }
+            else
+            {
+                scoreString.Append(score.ToString());
+            }
+            text.text = scoreString.ToString();
             if (PlayerPrefs.GetInt("Highscore", -1) < score)
             {
                 PlayerPrefs.SetInt("Highscore", score);
