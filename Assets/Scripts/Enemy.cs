90d4b7701da6bcda627247f3f364e5b68b2408e8
Max Fortna <33673420+thexammer@users.noreply.github.com>
2019-08-02 20:51:48 -0400

    Add basic Enemy control

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float speed = 4f;
+
+    public GameObject Target { get; set; }
+    public virtual string Name => "Default";
+    public bool Visible => GetComponent<SpriteRenderer>().enabled;
+
+    private Rigidbody2D rb;
+    
+    /// <summary>
+    /// Use for initializations.
+    /// </summary>
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    /// <summary>
+    /// Overload to change how movement behaves.
+    /// </summary>
+    protected virtual void FixedUpdate()
+    {
+        Vector3 direction = GetDirection();
+        rb.velocity = direction * speed;
+    }
+
+    /// <summary>
+    /// Determines the direction the enemy should go this fixed update. 
+    /// Feel free to change the speed here too.
+    /// </summary>
+    /// <returns>The direction the enemey should head.</returns>
+    protected virtual Vector3 GetDirection()
+    {
+        return (Target.transform.position - transform.position).normalized;
+    }
+
+    public virtual void SetVisibility(bool visible)
+    {
+        GetComponent<SpriteRenderer>().enabled = visible;
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private List<Enemy> enemies = new List<Enemy>();
+    private Dictionary<string, Enemy> enemyPrefabs;
+    private GameObject[] spawns;
+    private float timeFlashed = 0f;
+    
+    void Start()
+    {
+        enemyPrefabs = Resources.LoadAll<Enemy>("Prefabs/Enemies").ToDictionary(x => x.Name);
+        spawns = GameObject.FindGameObjectsWithTag("Enemy Spawn");
+    }
+    
+    void Update()
+    {
+        if (timeFlashed <= 0f)
+        {
+            Enemy closestEnemy = null;
+            float smallestDist = Mathf.Infinity;
+            foreach (Enemy enemy in enemies)
+            {
+                float dist = Vector3.Distance(enemy.transform.position, player.transform.position);
+                if (dist < smallestDist)
+                {
+                    smallestDist = dist;
+                    closestEnemy = enemy;
+                }
+                if (enemy.Visible)
+                {
+                    enemy.SetVisibility(false);
+                }
+            }
+            closestEnemy?.SetVisibility(true);
+        }
+        else
+        {
+            timeFlashed -= Time.deltaTime;
+        }
+    }
+
+    public void FlashAllEnemies(float seconds)
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.SetVisibility(true);
+        }
+        timeFlashed = seconds;
+    }
+
+    public void SpawnEnemy(string name, int spawnIndex)
+    {
+        Enemy e = GameObject.Instantiate(enemyPrefabs[name], spawns[spawnIndex].transform.position, Quaternion.identity);
+        e.Target = player;
+    }
+}
