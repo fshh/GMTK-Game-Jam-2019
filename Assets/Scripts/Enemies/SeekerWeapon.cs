79b7e82558647f78d93b12dce3f639ca8d87282b
fshh <ajknollmeyer@gmail.com>
2019-08-04 09:29:49 -0400

    Integrated seeker and shield attacks with animations

--- Assets/Scripts/Enemies/SeekerWeapon.cs
+++ Assets/Scripts/Enemies/SeekerWeapon.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class SeekerWeapon : EnemyWeapon
 {
-    [SerializeField] private float startingRot;
-    [SerializeField] private float endingRot;
+    [SerializeField] private float hitboxStartTime;
+    [SerializeField] private float hitboxEndTime;
+
+    private Collider2D hitbox;
+
+    private void Start() {
+        GetComponentInChildren<Animator>().SetTrigger("Attack");
+        hitbox = GetComponent<Collider2D>();
+        hitbox.enabled = false;
+    }
 
     public override void MoveWeapon()
     {
-        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Lerp(startingRot, endingRot, timeAlive / lifetime)));
+        hitbox.enabled = timeAlive > hitboxStartTime && timeAlive < hitboxEndTime;
     }
 }
---
+++ Assets/Scripts/Enemies/Shielder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shielder : Enemy
+{
+    [SerializeField] [Range(0f,1f)] private float attackMovementSpeedModifier = 0.5f;
+
+    protected override void AttackMovement() {
+        base.NormalMovement();
+        rb.velocity *= attackMovementSpeedModifier;
+    }
+}
